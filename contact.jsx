df4dc5aac1fe1b5792e7aa8f44ad12b43f8880c8
supritambig <127177031+supritambig@users.noreply.github.com>
2023-10-22 23:14:18 +0530

    Create contact.jsx

---
+++ contact.jsx
@@ -0,0 +1,12 @@
+import React from "contact";
+
+function Contact(){
+    return(
+        <>
+        <h1 className="text-primary">
+            Bootstrap in node</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora ipsum cupiditate voluptatum. Esse facilis minima perspiciatis quo vitae culpa aliquid.</p></>
+    )
+    
+}
+export default Contact;
