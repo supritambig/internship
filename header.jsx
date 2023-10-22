c4389892faeb1a3884deba8cd9ba3ea61b97bf77
supritambig <127177031+supritambig@users.noreply.github.com>
2023-10-22 23:17:59 +0530

    Create header.jsx

---
+++ header.jsx
@@ -0,0 +1,17 @@
+#box{
+    border: 5px solid green;
+    width: 500px;
+    margin-left: auto;
+    margin-right: auto;
+    padding: 5px;
+    text-align: center;
+    color: blue;
+    background-color: rgb(204,250,235);
+}
+
+img{
+    width: 275px;
+    margin-left: auto;
+    margin-right: auto;
+    display: block;
+}
