776e47ecb02b4bb66f8694e16bb6046b1fd4cd76
supritambig <127177031+supritambig@users.noreply.github.com>
2023-10-22 23:26:02 +0530

    Create head.jsx

---
+++ head.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Header.css"
+function Header(){
+
+    return(
+        <div id='box'>
+         <h1>Subash chandr Bose</h1>
+         <img src="https://images.edexlive.com/uploads/user/imagelibrary/2018/1/23/original/Netaji-Subash-Chandra-Bose1a.jpg" height={400}/>
+         </div>
+         );
+};
+export default Header
