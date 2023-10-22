f15bfdcd4b1ea8ae114882a2985fa4fc3ab268f8
supritambig <127177031+supritambig@users.noreply.github.com>
2023-10-22 23:28:55 +0530

    Create signup.js

---
+++ signup.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+function Signup() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignup = () => {
+    // Implement signup logic here
+  };
+
+  return (
+    <div>
+      <h2>Signup</h2>
+      <form>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="button" onClick={handleSignup}>
+          Sign Up
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Signup;
