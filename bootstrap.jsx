982e6d2dc01dc3250a0444ec059bf82edf5ece53
supritambig <127177031+supritambig@users.noreply.github.com>
2023-10-22 23:13:03 +0530

    Create bootstrap.jsx

---
+++ bootstrap.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "Bootstrap/dist/css/bootstrap.min.css"
+
+function Bootstrap(){
+    return(
+        <>
+        <h1 className="text-primary">
+            Bootstrap in node</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora ipsum cupiditate voluptatum. Esse facilis minima perspiciatis quo vitae culpa aliquid.</p></>
+    )
+    
+}
+export default Bootstrap;
