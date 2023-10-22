5e6f593810d094b676d1f03ba3dcb9d03d3acb15
supritambig <127177031+supritambig@users.noreply.github.com>
2023-10-22 23:15:16 +0530

    Create footer.jsx

---
+++ footer.jsx
@@ -0,0 +1,19 @@
+import React,{Component} from 'react'
+
+class Footer extends Component{
+    render(){
+     return(
+        <>
+        <hr/>
+        <h3>Contact</h3>
+        <p>
+            <b>Mobile:</b>7676586643
+        </p>
+        <p>
+            <b>Email:</b>kaushi@gmail.com
+        </p>
+        </>
+        );
+     }
+}
+export default Footer;
